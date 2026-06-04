57aed4c93a7a54edd0f7d5e7c87a4b11fc03f48b
pkity <124867086+pkity@users.noreply.github.com>
2026-06-04 10:19:47 +0800

    Update ownership command in download.
    Change ownership command to target the parent directory.

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ cd start
 git config core.sparseCheckout true
 echo $1/ >> .git/info/sparse-checkout
 git checkout master
-chown -R ecs-user:ecs-user $1
+chown -R ecs-user:ecs-user ../start
 cd $1/
 chmod +x *.sh
 ./send_ip_tip.sh
