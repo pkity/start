730e0d2409b748c20acf5a946ac7470948b9ff6d
陈战 <chenzhan002@chinasoftinc.com>
2026-06-04 14:22:25 +0800

    删除多余文件
    Signed-off-by: 陈战 <chenzhan002@chinasoftinc.com>

--- download.sh
+++
@@ -1,9 +0,0 @@
-git clone --no-checkout https://github.com/pkity/start.git
-cd start
-git config core.sparseCheckout true
-echo $1/ >> .git/info/sparse-checkout
-git checkout master
-chown -R ecs-user:ecs-user $1
-cd $1/
-chmod +x *.sh
-./send_ip_tip.sh
