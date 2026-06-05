9f8fe2a93a1ca67da9d6f1df5e0ee14aa950b16f
pkity <124867086+pkity@users.noreply.github.com>
2026-06-05 11:25:16 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
-# git clone --no-checkout https://github.com/pkity/start.git
-# cd start
-# git config core.sparseCheckout true
-# echo $1/ >> .git/info/sparse-checkout
-# git checkout master
-# chown -R ecs-user:ecs-user ../start
-# cd $1/
-# chmod +x *.sh
-# ./send_ip_tip.sh
+git clone --no-checkout https://github.com/pkity/start.git
+cd start
+git config core.sparseCheckout true
+echo $1/ >> .git/info/sparse-checkout
+git checkout master
+chown -R ecs-user:ecs-user ../start
+cd $1/
+chmod +x *.sh
+./send_ip_tip.sh
